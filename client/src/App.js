cf4f8eb35927b7681eea985d9cdb7a77753c3b1c
vladibo13 <vladibo13@gmail.com>
2024-03-08 15:31:45 +0200

    client init

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer";
+import NavigationBar from "./components/Navbar";
+
+
+const App = () => {
+  return (
+    <>
+      <NavigationBar />
+      <main>
+      <Footer/>
+      </main>
+    </>
+  );
+};
+
+export default App;
