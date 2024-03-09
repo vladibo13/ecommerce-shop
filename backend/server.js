53c552d44c7c7d39e6fa339250b5635b656b9167
vladibo13 <vladibo13@gmail.com>
2024-03-09 17:43:27 +0200

    mongodb database init

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 dotenv.config();
 const port = process.env.PORT || 5001;
 
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
